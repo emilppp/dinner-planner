81a6b60fb2abf649c77328a27c2026300ad7ce23
Emil <emiper@kth.se>
2017-01-31 17:14:20 +0100

    Hehehehej

--- app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
+++ app/src/main/java/se/kth/csc/iprog/dinnerplanner/android/MainActivity.java
@@ -1,9 +1,10 @@
 package se.kth.csc.iprog.dinnerplanner.android;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-
-import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -18,7 +19,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // Creating the view class instance
-        ExampleView mainView = new ExampleView(findViewById(R.id.this_is_example_view_id));
+
+        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getActionBar().setCustomView(R.layout.titlebar);
+
+        Button start = (Button) findViewById(R.id.welcomeBtn);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.choose_menu);
+            }
+        });
     }
 
 }
